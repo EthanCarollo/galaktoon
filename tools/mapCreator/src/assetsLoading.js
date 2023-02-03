4a65409d85b49684f4541861dbc312c29a6efaae
Jacky <deku.private@gmail.com>
2023-02-03 18:30:45 +0100

    Initializing the map creator. Initializing map creator for designer !

---
+++ tools/mapCreator/src/assetsLoading.js
@@ -0,0 +1,39 @@
+let tilesData = [];
+
+const loadAssets = () => {
+    fetch("../../../json/engineOne/tiles.json")
+        .then(rep => rep.json())
+        .then(rep => { 
+                tilesData = rep.data;  
+                loadImageAssets();                
+        })
+        .catch(error => { 
+            throw new Error("there is an issue with the ressource path");
+        })
+}
+
+const loadImageAssets = () => {
+
+    for(let i = 0; i < tilesData.length; i++)
+    {
+        tilesData[i].image = loadImage("../../../" + tilesData[i].path);
+    }
+    createDOM()
+}
+
+const createDOM = () => {
+    for(let j = 0; j<tilesData.length;j++)
+    {
+        let image = document.getElementById("innerTilesList").appendChild(document.createElement("image"))
+        image.classList.add("tile")
+        image.style.backgroundImage = "url(../../" + tilesData[j].path + ")";
+        image.addEventListener("mouseup", () => {
+            callbackTiles(j);
+        })
+    }
+
+}
+
+const callbackTiles = (id) => {
+    console.log(id)
+}
---
+++ tools/mapCreator/src/sketch.js
@@ -0,0 +1,14 @@
+function preload() {
+    loadAssets();
+  }
+  
+  
+function setup() {
+    canvas = createCanvas(window.innerWidth, window.innerHeight);
+    noSmooth();
+    frameRate(60);
+  }
+  
+function mouseClicked() {
+    
+  }
