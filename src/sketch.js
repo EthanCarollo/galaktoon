99190eb75532658b13009dd9268dbd93b998b009
Jacky <deku.private@gmail.com>
2023-02-15 20:55:17 +0100

    Setting up scene manager for the game

--- src/sketch.js
+++ src/sketch.js
@@ -13,22 +13,7 @@ function setup() {
 
 function draw() {
     cursor('auto') // Resetting the cursor to auto
-    if(ressourceIsLoaded === true){
-        switch(actualEngine){
-            case EngineOne :
-                // Code executing if actualEngine is the first one (in this case it's the 2D Top down Engine)
-                runEngineOne()
-                break;
-            case EngineTwo :
-                // Code executing if actualEngine is the second one
-                runEngineTwo()
-                break;
-            default :
-                throw new Error("Actual Engine isnt set");
-                break;
-        }
-        runInputManager();
-    }
+    sceneManager();
 }
 
 function windowResized() {
