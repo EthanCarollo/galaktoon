121b28cfd65837ce6e3e831696118046f0ae5f9b
Jacky <deku.private@gmail.com>
2023-02-16 13:41:11 +0100

    Updating npc data

--- src/engineOne/npcManager.js
+++ src/engineOne/npcManager.js
@@ -1,25 +1,19 @@
-let npcOnMap = [
-    {
-        id : 1,
-        position : [5, 7]
-    }
-]
 
 const displayNPCOnMap = (orientation = "back") => {
     switch(orientation){
         case "back":
-            for(let i = 0; i < npcOnMap.length; i++){
-                if(actualPlayerTile()[1] > npcOnMap[i].position[0])
+            for(let i = 0; i < playerOnMap.npcOnMap.length; i++){
+                if(actualPlayerTile()[1] >= playerOnMap.npcOnMap[i].position[0])
                   {
-                    displayNpc(npcOnMap[i])
+                    displayNpc(playerOnMap.npcOnMap[i])
                   }
             }
             break;
         case "front":
-            for(let i = 0; i < npcOnMap.length; i++){
-                if(actualPlayerTile()[1] <= npcOnMap[i].position[1])
+            for(let i = 0; i < playerOnMap.npcOnMap.length; i++){
+                if(actualPlayerTile()[1] <= playerOnMap.npcOnMap[i].position[1])
                   {
-                    displayNpc(npcOnMap[i])
+                    displayNpc(playerOnMap.npcOnMap[i])
                   }
             }
             break;
@@ -27,10 +21,11 @@ const displayNPCOnMap = (orientation = "back") => {
 }
 
 const displayNpc = (npc) => {
+    let spriteNpc = npcData[npc.id].spriteId
     let positionTemp = getCoordWithTileCoord(npc.position[0], npc.position[1]-1);
     positionTemp.x = positionTemp.x + cameraVector.x + playerVector.x
     positionTemp.y = positionTemp.y + cameraVector.y + playerVector.y
-    animationIdleSprite(positionTemp.x, positionTemp.y, playerSpriteSize, [0, 1], npc.id)
+    animationIdleSprite(positionTemp.x, positionTemp.y, playerSpriteSize, [0, 1], spriteNpc)
 }
 
 const interactWithNPC = (tileInteract) => {
