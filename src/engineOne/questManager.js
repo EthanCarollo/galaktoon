a5bd1e9e76af0f2986b463773991d37d63ee87c3
Ethew <118255407+JackyTuneur@users.noreply.github.com>
2023-03-12 18:15:11 +0100

    Adding quests progression

--- src/engineOne/questManager.js
+++ src/engineOne/questManager.js
@@ -22,10 +22,25 @@ const showQuestList = () => {
     let sizeYContainerQuest = 100;
     for(let i = 0; i < questList.length; i++)
     {
+        textSize(12)
+        textAlign(RIGHT, BOTTOM);
+        text(questList[i].currentProgression + " / " + questList[i].maxProgression, xPosQuestList, yStartList + sizeYContainerQuest* i, sizeQuestList, sizeYContainerQuest)
+
         textAlign(LEFT, TOP);
         textSize(15)
-        text(questList[i].name, xPosQuestList, yStartList + sizeYContainerQuest* i, sizeQuestList, sizeQuestList)
+        text(questList[i].name, xPosQuestList, yStartList + sizeYContainerQuest* i, sizeQuestList, sizeYContainerQuest)
+
         textSize(8)
-        text(questList[i].description, xPosQuestList, yStartList + sizeYContainerQuest* i + 25, sizeQuestList, sizeQuestList)
+        text(questList[i].description, xPosQuestList, yStartList + sizeYContainerQuest* i + 25, sizeQuestList, sizeYContainerQuest)
+    }
+}
+
+const addQuestProgression = (questIdProgression) => {
+    for(let i = 0; i< questList.length; i++)
+    {
+        if(questList[i].idQuestProgression === questIdProgression && questList[i].currentProgression < questList[i].maxProgression)
+        {
+            questList[i].currentProgression ++;
+        }
     }
 }
