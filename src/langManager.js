79576e5e872cc59e2faebf88bf915786ecf13264
Ethew <118255407+JackyTuneur@users.noreply.github.com>
2023-05-01 20:51:32 +0200

    Implemanting npc dialog lang

--- src/langManager.js
+++ src/langManager.js
@@ -1,6 +1,5 @@
 const LangPackage = {
-    fr : "./json/lang/langFR.json",
-    en : "./json/lang/langEN.json"
+    fr : "./json/lang/langFR.json"
 }
 
 const setLanguageOnStart = () => {
@@ -18,7 +17,7 @@ const loadSpecificLanguage = (language) => {
         })
         .catch(error => { 
             // error handling
-            throw new Error("there is an issue with the language pack : " + error + " // Selected lang : " + language.toUpperCase);
+            throw new Error("there is an issue with the language pack : " + error + " // Selected lang : " + language.toUpperCase());
 
         })
 }
@@ -33,13 +32,47 @@ const setLanguageOfEveryPrompts = (languageData) => {
             case "startMenu" : 
                 setLanguageOfStartingMenu(languageData["startMenu"])
                 break;
-            case "dialog" :
+            case "npc" :
+                setLanguageOfNpc(languageData["npc"])
                 break;
         }
     }
 }
 
 
+
+
+/**
+ * * setLanguage Function for the dialog menu
+ * @param {Object} npcPrompt this is an object that contains "start" or "exit"
+ */
+const setLanguageOfNpc = (npcPrompt) => {
+    /**
+     *  * Quadra for to set every language of every npc
+     */
+
+    for(let k = 0; k < npcPrompt.length; k++)
+    {
+        npcData[k].name = npcPrompt[k].name 
+        for(let j = 0; j < npcData[k].dialogs.length; j++)
+        {
+            for(let l = 0; l <npcData[k].dialogs[j].length;l++)
+            {
+                    npcData[k].dialogs[j][l].text = npcPrompt[k].dialogs[j][l].text
+            }
+                
+        }
+        console.log(npcData[k].dialogs)
+    }
+    
+}
+
+
+
+/**
+ * * setLanguage Function for the starting menu
+ * @param {Object} startMenuPrompt this is an object that contains "start" or "exit"
+ */
 const setLanguageOfStartingMenu = (startMenuPrompt) => {
     let languageKey = Object.keys(startMenuPrompt)
     for(let i = 0; i < languageKey.length; i++)
